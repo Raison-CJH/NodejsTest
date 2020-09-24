65ee1cfaa3004ac068a58455465744c675d263db
mc_memory@naver.com <mc_memory@naver.com>
2020-09-24 16:27:47 +0900

    Nodejs Test

---
+++ routes/test.js
@@ -0,0 +1,26 @@
+/**
+ * http://usejsdoc.org/
+ */
+
+var express = require('express');
+var router = express.Router();
+	
+router.post('/', function(req, res){
+	res.status(200).json(		
+		{
+			"name" : "NodejsTest",
+		}
+	);	
+});
+
+router.get('/test/:id', function(req, res){
+	let id = req.params.id;
+	res.status(200).json(		
+			{
+				"id" : id,
+			}
+	);	
+});
+
+
+module.exports = router;
